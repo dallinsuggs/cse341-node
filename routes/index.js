252ba445f0c84b39e3cc14e160ad87b01f46bf04
Dallin Suggs <dallin.suggs@gmail.com>
2022-04-29 12:48:47 -0600

    update again

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,7 @@
-const express = require('express');
-const router = express.Router();
-
-router.use('/contacts', require('./contacts'));
-
-module.exports = router;
-
-/* const routes = require('express').Router();
+const routes = require('express').Router();
 
 routes.get('/', (req, res) => {
   res.send('Nelli Suggs')
 });
 
-module.exports = routes */
+module.exports = routes
