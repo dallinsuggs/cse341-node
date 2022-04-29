bdf5e907e8508a7e788aebe04a7b0eefa20c8646
Dallin Suggs <dallin.suggs@gmail.com>
2022-04-29 14:58:38 -0600

    Clean up my DB code

---
+++ db/connect.js
@@ -0,0 +1,27 @@
+// .env variables DB_URI
+const dotenv = require('dotenv')
+dotenv.config();
+
+//database code
+const MongoClient = require('mongodb').MongoClient;
+
+let _client;
+let _collection;
+
+const initDatabase = () => {
+  MongoClient.connect(process.env.DB_URI, (err, client) => {
+    if (err) throw err;
+    _client = client;
+    _collection = client.db("contacts_db").collection("contacts");
+    console.log("DB Connected Successfully");
+  });
+};
+
+const getCollection = () => {
+  return _collection;
+};
+
+
+
+
+module.exports = { initDatabase, getCollection };
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,21 +1,14 @@
 const routes = require('express').Router();
+const connect = require('../db/connect');
 
 routes.get('/', (req, res) => {
 
-  const dotenv = require('dotenv')
-  dotenv.config();
-
-  const MongoClient = require('mongodb').MongoClient;
-  const uri = process.env.DB_URI;
-  MongoClient.connect(uri, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("contacts_db");
-    dbo.collection("contacts").find().toArray(function(err, result) {
+    connect.getCollection().find().toArray((err, result) => {
       if (err) throw err;
       res.json(result);
-      db.close();
+      console.log("Contacts Query Successful");
     });
-  });
+
 });
 
 module.exports = routes
