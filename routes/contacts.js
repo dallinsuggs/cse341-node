6378b524011540436555df8e9df3a606cbb66ffb
Dallin Suggs <dallin.suggs@gmail.com>
2022-05-05 08:34:52 -0600

    add CRUD

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,7 +1,13 @@
 const routes = require('express').Router();
 const connect = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
+const bodyParser= require('body-parser');
+const express = require('express');
 
+routes.use(bodyParser.urlencoded({ extended: true }));
+routes.use(express.json());
+
+//GET ALL CONTACTS
 routes.get('/', (req, res) => {
 
   const results = connect.getCollection().find();
@@ -13,9 +19,11 @@ routes.get('/', (req, res) => {
 
 });
 
+//GET 1 CONTACT
 routes.get('/:id', (req, res) => {
 
   const contactId = new ObjectId(req.params.id);
+
   const results = connect.getCollection().find({_id: contactId});
   
   results.toArray().then((documents) => {
@@ -23,6 +31,46 @@ routes.get('/:id', (req, res) => {
       console.log(`Returned Contact ${req.params.id}`);
   });
 
+});
+
+//POST 1 NEW CONTACT
+routes.post('/', (req, res) => {
+  console.log(req.body);
+  const newDoc = new Object(req.body);
+  const result = connect.getCollection().insertOne(newDoc)
+    .then(result => {
+      res.json(`New listing created with the following id: ${result.insertedId}`)
+    })
+    .catch(error => console.error(error));
+  console.log(`New listing created with the following id: ${result.insertedId}`);
+});
+
+//PUT UPDATE TO CONTACT
+routes.put('/:id', (req, res) => {
+
+  const contactId = new ObjectId(req.params.id);
+  
+  const newDoc = new Object(req.body);
+  
+  connect.getCollection().replaceOne({_id: contactId}, newDoc)
+    .then(result => {
+      res.json(`Updated listing for id ${req.params.id}`)
+    })
+    .catch(error => console.error(error));
+  console.log(`Updated listing for id ${req.params.id}`);
+});
+
+//DELETE CONTACT
+routes.delete('/:id', (req, res) => {
+
+  const contactId = new ObjectId(req.params.id);
+  
+  connect.getCollection().deleteOne({_id: contactId})
+    .then(result => {
+      res.json(`Deleted listing id ${req.params.id}`)
+    })
+    .catch(error => console.error(error));
+  console.log(`Deleted listing id ${req.params.id}`);
 });
 
 module.exports = routes
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const routes = require('express').Router();
+const express = require('express')
 
 routes.use('/', require('./home'));
 routes.use('/contacts', require('./contacts'));
+routes.use(express.json());
 
 module.exports = routes
